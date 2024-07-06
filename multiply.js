b03ef3ce35f7dc4b920f9dcdf251bab550cb22e1
katusabe scovia <scoviak588@gmail.com>
2024-07-06 17:23:22 +0300

    cycle 3 passed

--- multiply.js
+++ multiply.js
@@ -1,10 +1,14 @@
+// cycle 1
 // function multiply(a, b) {
 //     return 1;
 //   }
   
 //   module.exports = multiply;
   
+// cycle 2
 export default function multiply(a, b) {
     return a * b;
   }
+
+// cycle 3
   
--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
+// cycle 1
 // import { assert } from 'chai';
 // import multiply from '../multiply.js';
 
@@ -7,10 +8,24 @@
 //   });
 // });
 
+// cycle 2
+// import { assert } from 'chai';
+// import multiply from '../multiply.js';
+
+// describe('multiply', () => {
+//   it('should return 1 when multiplying 1 and 1', () => {
+//     assert.equal(multiply(1, 1), 1);
+//   });
+
+//   it('should return 4 when multiplying 2 and 2', () => {
+//     assert.equal(multiply(2, 2), 4);
+//   });
+// });
+
+// cycle 3
 
 import { assert } from 'chai';
 import multiply from '../multiply.js';
-
 describe('multiply', () => {
   it('should return 1 when multiplying 1 and 1', () => {
     assert.equal(multiply(1, 1), 1);
@@ -19,4 +34,9 @@ describe('multiply', () => {
   it('should return 4 when multiplying 2 and 2', () => {
     assert.equal(multiply(2, 2), 4);
   });
+
+  it('should return 9 when multiplying 3 and 3', () => {
+    assert.equal(multiply(3, 3), 9);
+  });
 });
+
