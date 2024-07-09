281ecb18ebd189e1917a4222485ec0b2fdc1ff98
katusabe scovia <scoviak588@gmail.com>
2024-07-09 14:52:17 +0300

    Cycle 1: Add initial test for tax calculator with earnings less than 12000

--- test/taxcalculator.test.js
+++ test/taxcalculator.test.js
@@ -2,7 +2,12 @@ import { assert } from 'chai';
 import calculateTax from '../taxcalculator.js';
 
 describe('Tax Calculator', () => {
-  it('should return 0 for earnings less than 12000', () => {
-    assert.equal(calculateTax(10000), 0);
+  // it('should return 0 for earnings less than 12000', () => {
+  //   assert.equal(calculateTax(10000), 0);
+  // });
+  it('should return 0 for earnings exactly 11999', () => {
+    assert.equal(calculateTax(11999), 0);
   });
+
+
 });
