9bdb37de64f29a69acbb3a85eeea7a606f4ccc0e
katusabe scovia <scoviak588@gmail.com>
2024-07-09 14:15:37 +0300

    factorial 2 passed

--- test/test1.js
+++ test/test1.js
@@ -2,13 +2,13 @@ import { assert } from 'chai';
 import { factorial } from '../factorial.js';
 
 describe('factorial', () => {
-  it('should return 1 when n is 1', () => {
-    assert.equal(factorial(1), 1);
-  });
-
-//   it('should return 2 when n is 2', () => {
-//     assert.equal(factorial(2), 2);
+//   it('should return 1 when n is 1', () => {
+//     assert.equal(factorial(1), 1);
 //   });
+
+  it('should return 2 when n is 2', () => {
+    assert.equal(factorial(2), 2);
+  });
 
 //   it('should return 6 when n is 3', () => {
 //     assert.equal(factorial(3), 6);
