a491a63feef4c02a233f107788994ec5a9dd710e
katusabe scovia <scoviak588@gmail.com>
2024-07-09 14:55:49 +0300

    Cycle 3: Add test and implementation for earnings between 12000 and 36000

--- taxcalculator.js
+++ taxcalculator.js
@@ -1,6 +1,17 @@
+// function calculateTax(earnings) {
+//     return 0;
+//   }
+  
+// export default calculateTax;
+  
+//cycle 3
 function calculateTax(earnings) {
+  if (earnings < 12000) {
     return 0;
+  } else if (earnings <= 36000) {
+    return (earnings - 12000) * 0.2;
   }
-  
+  return 0;
+}
+
 export default calculateTax;
-  
--- test/taxcalculator.test.js
+++ test/taxcalculator.test.js
@@ -5,9 +5,11 @@ describe('Tax Calculator', () => {
   // it('should return 0 for earnings less than 12000', () => {
   //   assert.equal(calculateTax(10000), 0);
   // });
-  it('should return 0 for earnings exactly 11999', () => {
-    assert.equal(calculateTax(11999), 0);
+  // it('should return 0 for earnings exactly 11999', () => {
+  //   assert.equal(calculateTax(11999), 0);
+  // });
+
+  it('should return 1600 for earnings of 20000', () => {
+    assert.equal(calculateTax(20000), 1600);
   });
-
-
 });
