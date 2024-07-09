a96a306e7727fed90cd65074c7295a135ef3ab14
katusabe scovia <scoviak588@gmail.com>
2024-07-09 21:07:10 +0300

    added fibonnaci4

--- test/fibonacci.test.js
+++ test/fibonacci.test.js
@@ -9,8 +9,8 @@ function testFibonacci() {
     // assert.strictEqual(fibonacci(1), 1);
     // console.log('Test passed: Fibonacci of 1 is 1');
 
-    assert.strictEqual(fibonacci(2), 1);
-    console.log('Test passed: Fibonacci of 2 is 1');
+    // assert.strictEqual(fibonacci(2), 1);
+    // console.log('Test passed: Fibonacci of 2 is 1');
 
     // assert.strictEqual(fibonacci(3), 2);
     // console.log('Test passed: Fibonacci of 3 is 2');
@@ -18,8 +18,8 @@ function testFibonacci() {
     // assert.strictEqual(fibonacci(4), 3);
     // console.log('Test passed: Fibonacci of 4 is 3');
     
-    // assert.strictEqual(fibonacci(5), 5);
-    // console.log('Test passed: Fibonacci of 5 is 5');
+    assert.strictEqual(fibonacci(5), 5);
+    console.log('Test passed: Fibonacci of 5 is 5');
 }
 
 testFibonacci();
