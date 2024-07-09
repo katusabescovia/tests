591f5630d8112120be837360438220e5050b7fe1
katusabe scovia <scoviak588@gmail.com>
2024-07-09 14:16:57 +0300

    third  factorial passed

--- test/test1.js
+++ test/test1.js
@@ -6,13 +6,13 @@ describe('factorial', () => {
 //     assert.equal(factorial(1), 1);
 //   });
 
-  it('should return 2 when n is 2', () => {
-    assert.equal(factorial(2), 2);
-  });
-
-//   it('should return 6 when n is 3', () => {
-//     assert.equal(factorial(3), 6);
+//   it('should return 2 when n is 2', () => {
+//     assert.equal(factorial(2), 2);
 //   });
+
+  it('should return 6 when n is 3', () => {
+    assert.equal(factorial(3), 6);
+  });
 
 //   it('should return 24 when n is 4', () => {
 //     assert.equal(factorial(4), 24);
